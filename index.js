a8d11890c6db24bac5077be4f09d30a88d72ea35
chris-wamae <chriswamae123@gmail.com>
2022-12-16 16:09:24 +0300

    Created function to show and hide sign up form

--- index.js
+++ index.js
@@ -3,3 +3,18 @@ function fetchData(callBack) {
       .then((response) => response.json())
       .then((cardsData) => callBack(cardsData["data"]));
   }
+
+  function formDisplay() {
+    document.querySelector("section#sign-up").style.display = "none";
+    let signUp = document.querySelector("p#sign-up-nav");
+    signUp.addEventListener("click", function () {
+      document.querySelector("section#sign-up").setAttribute("style", "");
+      let signUpSubmit = document.querySelector("button#sign-up-button");
+      signUpSubmit.addEventListener("click", function (event) {
+        event.preventDefault();
+        document.querySelector("section#sign-up").style.display = "none";
+      });
+    });
+  }
+  formDisplay();
+  
