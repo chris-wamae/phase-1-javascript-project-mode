618bed492e0bf61136d5f77bb9b29c9e581e0625
chris-wamae <chriswamae123@gmail.com>
2022-12-16 16:11:21 +0300

    Created function that allows a user to go through images and select an image to be their avatar

--- index.js
+++ index.js
@@ -25,3 +25,47 @@ function fetchData(callBack) {
     setTimeout(alertTimeout, 10);
   }
   
+  //selects the next Image when choosing an avatar
+
+function imageSelection(apiArray) {
+    let firstImage = 0;
+    let currentImage = document.querySelector("img#player-avatar");
+    currentImage.setAttribute("src", apiArray[firstImage]["displayIcon"]);
+    console.log(apiArray);
+  
+    let nextImage = document.querySelector("button#next-image");
+    nextImage.addEventListener("click", function () {
+      firstImage = firstImage + 1;
+      currentImage.setAttribute("src", apiArray[firstImage]["displayIcon"]);
+    });
+  
+    let previousImage = document.querySelector("button#previous-image");
+    previousImage.addEventListener("click", function () {
+      if (firstImage > 0) {
+        firstImage = firstImage - 1;
+      } else {
+        firstImage = 0;
+      }
+      currentImage.setAttribute("src", apiArray[firstImage]["displayIcon"]);
+    });
+  
+    let selectImage = document.querySelector("button#select-image");
+    let divNumber = 0;
+    selectImage.addEventListener("click", function () {
+      let selectedImage = document.createElement("img");
+      let playerDiv = document.createElement("div");
+      playerDiv.setAttribute("id", `playerDiv${divNumber}`);
+      divNumber = divNumber + 1;
+      console.log(`Image selection Div number:${divNumber}`);
+      document.querySelector("p#join-hall-of-fame").style.display = "none";
+  
+      selectedImage.setAttribute("src", apiArray[firstImage]["displayIcon"]);
+      let imageSection = document.querySelector("section#hall-of-fame");
+      playerDiv.append(selectedImage);
+      imageSection.append(playerDiv);
+      selectImage.style.display = "none";
+    });
+  }
+  
+  fetchData(imageSelection);
+  
